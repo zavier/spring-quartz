17f0c41fa6fa841e0dadf418b201830cadf246e5
zheng <765324639@qq.com>
2018-04-13 13:46:46 +0800

    添加 Spring MVC

---
+++ src/main/java/example/web/DemoController.java
@@ -0,0 +1,12 @@
+package example.web;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+    @RequestMapping("/demo")
+    public String demo() {
+        return "This is demo";
+    }
+}
